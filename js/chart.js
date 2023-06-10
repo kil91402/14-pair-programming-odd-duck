c2a76f1e03ddc49ed47158bf354e7243061791e0
Ajamu Page <kil91402@gmail.com>
2023-06-10 15:27:36 -0500

    Added chart code

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-let canvasElem = document.getElementById('chart')
+let canvasElem = document.getElementById("chart");
 
 /* TODO:
  * - Instantiate a new AppState
@@ -10,7 +10,44 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
-function renderChart() {
+let chart;
+function displayChart(data) {
+  if (chart !== undefined) {
+    chart.destroy();
+  }
+  let labels = getLabelData(data);
+  let votes = getVoteData(data);
+  let ctx = document.getElementById("chart");
+  let dataObj = {
+    type: "bar",
+    data: {
+      labels: ["product1", "product2", "product3"],
+      datasets: [
+        {
+          label: "votes",
+          data: votes,
+        },
+      ],
+    },
+  };
+  let chart = new Chart(ctx, dataObj);
 }
+
+function getVoteData(productIndexArray) {
+  let votes = [];
+  for (let productIndex of productIndexArray) {
+    votes.push(productIndex.voteCount);
+  }
+  return votes;
+}
+
+function getLabelData(productIndexArray) {
+  let labels = [];
+  for (let product of productIndexArray) {
+    labels.push(productIndex.name);
+  }
+  return labels;
+}
+function renderChart() {}
 
 renderChart();
